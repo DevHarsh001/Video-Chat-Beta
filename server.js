e7f2da818cb458bc0db71b25db8e838f01f763c8
DevHarsh001 <harshtiwari6060@gmail.com>
2025-05-29 20:40:54 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const http = require("http");
+const { v4: uuidv4 } = require("uuid");
+const socketIo = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(express.static("public"));
+
+// Generate a unique room id and redirect
+app.get("/start", (req, res) => {
+  const roomId = uuidv4();
+  res.redirect(`/room/${roomId}`);
+});
+
+// Serve room page
+app.get("/room/:roomId", (req, res) => {
+  res.sendFile(__dirname + "/public/room.html");
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+
+    socket.on("signal", (data) => {
+      socket.to(roomId).emit("signal", data);
+    });
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
